f60b34588b73ac577a4a533009d969b63924ee92
daniels0009 <36423740+daniels0009@users.noreply.github.com>
2018-04-16 15:15:53 -0500

    Add files via upload

---
+++ recorrido.py
@@ -0,0 +1,28 @@
+class Nodo():
+    def __init__(self, valor, izq = None, der = None):
+        self.valor= valor
+        self.izquierda = izq
+        self.derecha=der
+        
+def posorden(arbol):
+    if arbol==None:
+        return ""
+    else:
+        return posorden(arbol.izquierda)+posorden(arbol.derecha)+arbol.valor
+def inorden(arbol):
+    if arbol==None:
+        return ""
+    else:
+        return inorden(arbol.izquierda)+arbol.valor+inorden(arbol.derecha)
+    
+def preorden(arbol):
+     if arbol==None:
+        return ""
+     else:
+        return arbol.valor+preorden(arbol.izquierda)+preorden(arbol.derecha)
+    
+   
+arbol = Nodo('1500000 ',Nodo('2000004 '),Nodo('180000 ',Nodo('100 '),Nodo('10000 ')))
+print("pre: "+preorden(arbol))
+print("in: "+inorden(arbol))
+print("pos: "+posorden(arbol))
